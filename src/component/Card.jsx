bb850ad197f4b4172ddfbfad739c2a3fc75b158b
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-04-04 21:26:44 +0600

    task

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({movie}) => {
     return (
         <div className="card  bg-base-100 shadow-xl">
   <figure className="px-10 pt-10">
-    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+    <img src={bannerURL} alt="Shoes" className="rounded-xl " />
   </figure>
   <div className="card-body items-center text-center">
     <h2 className="card-title">{movieName}</h2>
